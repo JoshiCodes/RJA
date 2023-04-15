8115f4ce50ac45ac9e276924416688c4efc39c1b
Joshua <joshua@joshicodes.de>
2023-04-16 01:08:56 +0200

    Update CacheTest.java
    made the comments look a bit cleaner

--- src/test/java/de/joshicodes/rja/cache/CacheTest.java
+++ src/test/java/de/joshicodes/rja/cache/CacheTest.java
@@ -15,16 +15,16 @@ public class CacheTest {
 
         assertTrue(cache.contains("test"));
 
-        cache.add("test2", 1000);                        // put Object with lifetime of 1 second
+        cache.add("test2", 1000);                                      // put Object with lifetime of 1 second
         assertTrue(cache.contains("test2"));                           // Make sure it's in cache
-        Thread.sleep(1005);                                   // Wait 1,005 seconds
+        Thread.sleep(1005);                                            // Wait 1,005 seconds
         assertFalse(cache.contains("test2"));                          // Make sure lifetime works
         assertFalse(cache.stream().anyMatch(s -> s.equals("test2")));  // Make sure not in stream/list
 
         assertTrue(cache.contains("test"));                           // Make sure it's still in cache
         assertTrue(cache.stream().anyMatch(s -> s.equals("test")));   // Make sure it's still in stream/list
 
-        cache.remove("test");                                     // Remove Object
+        cache.remove("test");                                         // Remove Object
 
         assertFalse(cache.contains("test"));                         // Make sure it's not in cache
         assertFalse(cache.stream().anyMatch(s -> s.equals("test"))); // Make sure it's not in stream/list
