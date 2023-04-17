259a679489e01c81d8c8e5f9f303ce07b3bf62ae
Joshua <joshua@joshi.codes>
2023-04-17 22:20:12 +0200

    Changed Masquerade to record

--- src/main/java/de/joshicodes/rja/object/user/Masquerade.java
+++ src/main/java/de/joshicodes/rja/object/user/Masquerade.java
@@ -5,38 +5,16 @@ import de.joshicodes.rja.RJA;
 import de.joshicodes.rja.object.RJAColor;
 import de.joshicodes.rja.util.JsonUtil;
 
-public class Masquerade {
-
-    private final String name;
-    private final String avatar;
-    private final RJAColor color;
-
-    public Masquerade(String name, String avatar, RJAColor color) {
-        this.name = name;
-        this.avatar = avatar;
-        this.color = color;
-    }
+public record Masquerade(String name, String avatar, RJAColor color) {
 
     public static Masquerade from(RJA rja, JsonObject object) {
-        if(object == null) return null;
+        if (object == null) return null;
 
         final String name = JsonUtil.getString(object, "name", null);
         final String avatar = JsonUtil.getString(object, "avatar", null);
         final RJAColor color = null; // TODO
 
         return new Masquerade(name, avatar, color);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public RJAColor getColor() {
-        return color;
     }
 
     public JsonObject toJson() {
