2895abcee09913cc54827cc8f9d68826b866934f
Joshua <joshua@joshi.codes>
2023-04-21 21:54:04 +0200

    Added MessageReceiver#sendFile and MessageReceiver#sendEmbeds(MessageEmbed...)

--- src/main/java/de/joshicodes/rja/object/message/MessageReceiver.java
+++ src/main/java/de/joshicodes/rja/object/message/MessageReceiver.java
@@ -1,8 +1,11 @@
 package de.joshicodes.rja.object.message;
 
 import de.joshicodes.rja.RJA;
+import de.joshicodes.rja.object.Attachment;
 import de.joshicodes.rja.object.message.embed.MessageEmbed;
 import de.joshicodes.rja.rest.message.MessageSendAction;
+
+import java.util.List;
 
 public abstract class MessageReceiver {
 
@@ -16,5 +19,14 @@ public abstract class MessageReceiver {
     public MessageSendAction sendEmbeds(MessageEmbed embed) {
         return new MessageSendAction(getRJA(), getId()).setEmbeds(embed);
     }
+
+    public MessageSendAction sendEmbeds(MessageEmbed... embeds) {
+        return new MessageSendAction(getRJA(), getId()).setEmbeds(embeds);
+    }
+
+    public MessageSendAction sendFile(Attachment attachment) {
+        return new MessageSendAction(getRJA(), getId()).setAttachments(List.of(attachment));
+    }
+
 
 }
